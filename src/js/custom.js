1703d30610f051ae81f196527cc1eceb377a144a
nadiatapm <nadiatap@gmail.com>
2017-01-07 15:23:12 -0300

    arreglo cuidades

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 	// tu codigo va acá
+	cuidades = get_regiones();
+	origen = $('#origen');
+	destino = $('#destino');
+	$.each(cuidades, function( index , value){
+
+		origen.append($("<option/>").val(index).text(cuidades[index].name));
+		destino.append($("<option/>").val(index).text(cuidades[index].name));
+	});
 });
 $(document).ready(function(){
 	$('.tooltipped').tooltip({delay: 50});
